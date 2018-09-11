e9c2390c5a46eae55d2a9acb100ff2b2387997f9
IsaiasLedezma <elkillerpapi@gmail.com>
2018-09-10 19:02:03 -0700

    Create examen.scala

---
+++ examen.scala
@@ -0,0 +1,29 @@
+// EJERCICIO 1 
+def espar (num: Int): Boolean= {
+     if(num % 2 == 0){
+     return true}
+     return false}
+     
+     val numeroingresada = 4
+     println(espar(numeroingresada))
+     
+ //EJERCICIO 2
+  def ESONOES (n: Int): Boolean={
+     if (n % 2 == 0) {
+     return true}
+       return false
+       }
+       
+       val l = List(1,2,3,4,5,6,7,8,9,10)
+       var i = 0
+   for (i <- l){
+     if (i<=i){
+     println("Este numero NO ES PAR = "+a)
+     println(ESONOES(a))
+      }
+    }
+   
+ //EJERCICIO 3
+ 
+
+       
