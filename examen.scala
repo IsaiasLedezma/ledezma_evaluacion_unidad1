00a3e75a57ba8b7911ecd8c4e9a6c3826afd5bc7
IsaiasLedezma <elkillerpapi@gmail.com>
2018-09-12 18:27:35 -0700

    Update examen.scala

--- examen.scala
+++ examen.scala
@@ -18,12 +18,35 @@ def espar (num: Int): Boolean= {
        var i = 0
    for (i <- l){
      if (i<=i){
-     println("Este numero NO ES PAR = "+a)
-     println(ESONOES(a))
+     println("Este numero NO ES PAR = "+i)
+     println(ESONOES(i))
       }
     }
    
  //EJERCICIO 3
- 
+val isaias = List(1,4,5,7,7)
+val s = 0
+def ejercicio3(n:Int):Int={
+     if (n == 7){
+          s = s+n*2
+     }
+     else{
+          s = s+n
+     }
+     return ejercicio3
+}
+ejercicio3(isaias(s))
+
+//Ejercicio 4
+
+def ejerci4(lista: List(Int):Boolean)
+
+
+//Ejercicio 5
+def palindromo(palabra:String):Boolean={
+     return(palabra==palabra.reverse)}
+
+val palabra2 = "anna"
+val palabra3 = "isaias"
 
        
